de20aead0b89341f3e0968ce3251bc6d581f8f02
Mark Deegan <mark.deegan@tudublin.ie>
2023-10-25 16:50:07 +0100

    Adding Hello World.java

---
+++ ex01/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+Armand Ediae <br>
+Wednesday<br>
+20231025 1200 <br>
+HelloWorld <br>
+Sample to demonstrate basic java concepts
+*/
+public class HelloWorld
+{ // start declaration of the class HelloWorld
+	public static void main(String[] args)
+    	{ // start declaration of main method
+		System.out.println("Hello World!"); // standard Hello World! greeting
+	} // end declaration of main method
+} // end declaration of class HelloWorld
