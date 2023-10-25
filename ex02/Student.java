657464044174a6b7b1ba242129f93a9020fe30b3
Mark Deegan <mark.deegan@tudublin.ie>
2023-10-25 16:52:40 +0100

    Adding Person.java and Student.java

---
+++ ex02/Student.java
@@ -0,0 +1,13 @@
+/** 
+Mark Deegan <br>
+20231025 1652 <br>
+Studebnt class <br>
+Sample code to demonstrate class declaration and inheritance (extending classes) <br>
+*/
+public class Student extends Person
+{ // start declaration of public class Student, this class extends (inherits from) class Person
+	public static void main(String[] args)
+	{ // start declaration of the main method
+		System.out.println("Hello, I am a Student!"); // print a simple message
+	} // end declaration of the main method
+} // end declaration of class Student
